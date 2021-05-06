36543cefa9717ad278773176cb77e304b964d555
appdess <adess@vmware.com>
2021-05-06 16:12:00 +0200

    prometheus+pushgateway

--- prometheus/install-prom-operator.sh
+++ prometheus/install-prom-operator.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-
+kubectl create ns monitoring
 # add prom community repo:
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 # install pushgateway for metrics from Tito
 helm install pushgateway prometheus-community/prometheus-pushgateway --set serviceMonitor.enabled=true --set serviceMonitor.namespace=default --set serviceMonitor.honorLabels=true --set service.type=LoadBalancer --set serviceMonitor.interval="2s"
-
+helm install pushgateway prometheus-community/prometheus-pushgateway -f values-pushgw.yaml
 #Install Prom operator
-helm install prometheus prometheus-community/kube-prometheus-stack -f values.yaml
+helm install prometheus prometheus-community/kube-prometheus-stack -f values-prom.yaml
